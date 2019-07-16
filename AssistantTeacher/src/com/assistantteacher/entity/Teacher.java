ba3c70ae072c2757a9b0cc9c8d3242ae4fd23aee
1 <1@LAPTOP-OK8TN1KD>
2019-07-16 16:34:20 +0800

    Teacher

---
+++ AssistantTeacher/src/com/assistantteacher/entity/Teacher.java
@@ -0,0 +1,71 @@
+package com.assistantteacher.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+
+@Entity
+@Transactional
+@Table(name="Teacher")
+public class Teacher implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	@Column(name="Name")
+	private String name;
+	@Column(name="Email")
+	private String email;
+	@Column(name="Department")
+	private String department;
+	@Column(name="Role")
+	private String role;
+	@Column(name="Phone No")
+	private String phno;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+}
