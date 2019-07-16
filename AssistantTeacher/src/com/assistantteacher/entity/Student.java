532a86c91010f049debafb0406753b0ef457a5c1
1 <1@LAPTOP-OK8TN1KD>
2019-07-16 16:29:08 +0800

    Student entity

---
+++ AssistantTeacher/src/com/assistantteacher/entity/Student.java
@@ -0,0 +1,71 @@
+package com.assistantteacher.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	@Column(name="Name")
+	private String name;
+	@Column(name="RollNo")
+	private String rollNo;
+	@Column(name="Email")
+	private String email;
+	@Column(name="Password")
+	private String password;
+	@Column(name="PhNo")
+	private String phno;
+	@Column(name="Gender")
+	private String gender;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(String rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
